ce2ccc2d89a7cd02b4bc3da644502d7f43c24b5c
Kul-shingh <neerazmgr3@gmail.com>
2025-10-19 21:27:42 +0545

    add a java file

---
+++ practice1.java
@@ -0,0 +1,13 @@
+/*28. You are asked to write a discount system for a Mart, which providesservices and sells different
+products. It has 2 types of members: Premium and General. Premium customer gets 5%
+discount on purchase of &lt; = $1000 amount of products and 10% discount on purchase of &gt;
+$1000 amount of products. Meanwhile, Premium customer gets no discount on purchase of &lt; =
+$1000 amount of products and 3% discount on purchase of &gt; $1000 amount of products. Your
+system shall consist of three classes: Customer, Discount and Purchase. It shall compute the
+
+total bill if a customer purchase $x of products. Also write a test program to exercise all the
+classes. */
+
+public class practice1 {
+    
+}
